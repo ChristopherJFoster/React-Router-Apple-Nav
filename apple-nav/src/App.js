8f2e00956359281763693284c1753e726cb0c199
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-02-27 00:52:12 -0500

    Set up some basic routing. Copied and imported icon data. Installed style-components library. Created component files.

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import NavData from "./nav_data/NavData";
+import NavWrapper from "./components/NavWrapper";
+// import Nav from "./components/Nav";
+// import SubNav from "./components/SubNav";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      NavData: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      NavData: NavData
+    });
+  }
+
   render() {
+    console.log(this.state.NavData);
     return (
-      <div className="App">
-        <header className="App-header" />
-      </div>
+      <Route
+        path="/"
+        render={routeProps => (
+          <NavWrapper {...routeProps} NavData={this.state.NavData} />
+        )}
+      />
     );
   }
 }
